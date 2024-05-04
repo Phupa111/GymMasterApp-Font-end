c39bd628480cf9cf9a74241c81aaf4bf7a7c7fd6
Phupa111 <santapupa111@gmail.com>
2024-05-04 13:01:08 +0700

    comit1

---
+++ gym_master_fontend/lib/model/TabelModel.dart
@@ -0,0 +1,53 @@
+// To parse this JSON data, do
+//
+//     final tabelModel = tabelModelFromJson(jsonString);
+
+import 'dart:convert';
+
+List<TabelModel> tabelModelFromJson(String str) => List<TabelModel>.from(json.decode(str).map((x) => TabelModel.fromJson(x)));
+
+String tabelModelToJson(List<TabelModel> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class TabelModel {
+    int tid;
+    int uid;
+    String couserName;
+    int times;
+    int gender;
+    int level;
+    String description;
+    int isCreateByAdmin;
+
+    TabelModel({
+        required this.tid,
+        required this.uid,
+        required this.couserName,
+        required this.times,
+        required this.gender,
+        required this.level,
+        required this.description,
+        required this.isCreateByAdmin,
+    });
+
+    factory TabelModel.fromJson(Map<String, dynamic> json) => TabelModel(
+        tid: json["tid"],
+        uid: json["uid"],
+        couserName: json["couserName"],
+        times: json["times"],
+        gender: json["gender"],
+        level: json["level"],
+        description: json["description"],
+        isCreateByAdmin: json["isCreateByAdmin"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "tid": tid,
+        "uid": uid,
+        "couserName": couserName,
+        "times": times,
+        "gender": gender,
+        "level": level,
+        "description": description,
+        "isCreateByAdmin": isCreateByAdmin,
+    };
+}
--- gym_master_fontend/lib/screen/hom_page.dart
+++ gym_master_fontend/lib/screen/hom_page.dart
@@ -1,52 +1,34 @@
 import 'dart:developer';
 
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:dio/dio.dart';
-import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:gym_master_fontend/model/TabelModel.dart'; // Add import for TabelModel
 import 'package:gym_master_fontend/model/UserModel.dart';
+import 'package:gym_master_fontend/screen/Tabel/adminTabel_page.dart';
 import 'package:gym_master_fontend/screen/login_page.dart';
 
 class HomePage extends StatefulWidget {
   int? uid = 0;
-  HomePage({Key? key,this.uid }) : super(key: key);
+  HomePage({Key? key, this.uid}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  auth.User? currentUser;
- int uid =0;
- 
-  late Future<void> loadData ;
-   GetStorage gs = GetStorage();
-   late UserModel  userModel = gs.read('userModel'); 
+  List<TabelModel> adminTabels = [];
+
+  late Future<void> loadData;
+  GetStorage gs = GetStorage();
+  late UserModel userModel = gs.read('userModel');
+
   @override
   void initState() {
     super.initState();
-    // Listen to the authentication state changes
-    auth.FirebaseAuth.instance
-        .authStateChanges()
-        .listen((auth.User? updatedUser) {
-      setState(() {
-        currentUser = updatedUser;
-      });
-    });
-
-   
- 
-  }
-
-  void logOut() async {
-    try {
-      await auth.FirebaseAuth.instance.signOut();
-      Get.to(const LoginPage());
-    } catch (e) {
-      // Handle sign-out errors
-      print("Error signing out: $e");
-    }
+    loadData = loadDataAsync();
   }
 
   @override
@@ -54,43 +36,95 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home"),
-        automaticallyImplyLeading: currentUser != null ? false : true,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: ElevatedButton(
-              onPressed: logOut,
-              child: const Text("Log Out"),
-            ),
-          ),
-          const SizedBox(height: 20),
-          Center(
-            child: Text(
-              currentUser != null
-                  ? "Logged in as ${currentUser!.email} ${userModel.user.uid} ${userModel.user.isDisbel}"
-                  : "Guest",
-            ),
-          ),
-        ],
+      body: FutureBuilder<void>(
+        future: loadData,
+        builder: (context, AsyncSnapshot<void> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            return SingleChildScrollView(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text("Training Course",
+                                style: TextStyle(fontSize: 16)),
+                            TextButton(
+                                onPressed: () {
+                                  Get.to(const AdminTabelPage());
+                                },
+                                child: const Text(
+                                  "ดูเพิ่มเติม",
+                                  style: TextStyle(
+                                    color: Color(0xFFFFAC41),
+                                  ),
+                                ))
+                          ],
+                        ),
+        CarouselSlider(
+  items: adminTabels.map((adminTabel) => Padding(
+    padding: const EdgeInsets.all(12.0),
+    child: InkWell(
+      onTap: () {
+        
+      },
+      child: Container(
+        decoration: BoxDecoration(color: Colors.orange),
+        width: 400,
+        child: Column(
+          children: [
+            Text(adminTabel.couserName.toString()),
+            // Add more widgets here as needed
+          ],
+        ),
+      ),
+    ),
+  )).toList(),
+  options: CarouselOptions(
+    height: 200,
+    autoPlay: true,
+    enlargeCenterPage: true,
+    aspectRatio: 16 / 9,
+    autoPlayCurve: Curves.fastOutSlowIn,
+    enableInfiniteScroll: true,
+    viewportFraction: 0.8,
+  ),
+),
+
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }
+        },
       ),
     );
   }
 
-  // Future<void> loadDataAsync() async {
-  //   if (currentUser == null) {
-  //     return; // Return early if currentUser is null
-  //   }
+  Future<void> loadDataAsync() async {
+    final dio = Dio();
 
-  //   final dio = Dio();
-  //   final response = await dio.get(
-  //       'http://192.168.1.125:8080/user/selectFromEmail/${currentUser!.email}');
-
-  //   if (response.statusCode == 200) {
-  //     await GetStorage().write('uid', userModel.uid);
-  //     await GetStorage().write('role', userModel.role);
-  //     log("Success");
-  //   }
-  // }
+    try {
+      final response =
+          await dio.get('http://192.168.1.111:8080/Tabel/getAdminTabel');
+      final jsonData =
+          response.data as List<dynamic>; // Assuming the response is a list
+      adminTabels = jsonData.map((item) => TabelModel.fromJson(item)).toList();
+      log(adminTabels.length.toString());
+    } catch (e) {
+      log('Error fetching data: $e');
+      throw Exception('Error fetching data');
+    }
+  }
 }
